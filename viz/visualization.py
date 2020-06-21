1d1fd18390bfa6bb035bbf36377ccde1793e01a2
Matthew Sand <matthew@Matthews-iMac.local>
2020-06-20 22:59:04 -0400

    Idk update the thing

--- viz/visualization.py
+++ viz/visualization.py
@@ -1,15 +1,18 @@
-import pygame
+import pygame, pathlib
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption('test')
+pygame.display.set_caption('Vizualizer')
 clock = pygame.time.Clock()
 crashed = False
+zoombaSprite = pygame.image.load(pathlib.Path().absolute + "/Zoomba.png")
+def zoomba(x,y):
+    gameDisplay.blit(zoombaSprite,(x,y))
 
 while not crashed:
+    zoomba(100,100)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             crashed = True
 
-        print(event)
     pygame.display.update()
     clock.tick(60)
