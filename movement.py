5b96aebecca7a53201b5e3eae38faa0857cee17a
Matthew Sand <matthew@Matthews-iMac.local>
2020-07-02 19:17:25 -0400

    Did Some Turn Stuff But it Turns Too Much

--- movement.py
+++ movement.py
@@ -1,5 +1,5 @@
 import json, time, sys, argparse
-import accelerometer
+
 
 class movingClass:          #basically just want organized static variables
     pass
@@ -28,22 +28,45 @@ def readJson(fname):
     return output
 
 def turnRight(deg):
-    time = findturnTime(deg)
+    turnTime = findTurnTime(deg)
+    t_end = time.time() + turnTime
+    changeInDegrees = deg/turnTime
+    starttime = time.time()
+    while time.time() < t_end:
+        turnOnLeftWheels("forward")
+        turnOnRightWheels("backwards")
+        positionData["rotation"] = positionData["rotation"] + changeInDegrees
+        writeJson("positionData.json",positionData)
+        if positionData["rotation"] > 360:
+            positionData["rotation"] = positionData["rotation"] - 360
+
 
 def turnLeft(deg):
-    time = findturnTime(deg)
+    turnTime = findTurnTime(deg)
+    t_end = time.time() + turnTime
+    changeInDegrees = deg/turnTime
+    while time.time() < t_end:
+        turnOnLeftWheels("forward")
+        turnOnRightWheels("backwards")
+        positionData["rotation"] = positionData["rotation"] - changeInDegrees
+        writeJson("positionData.json",positionData)
+        if positionData["rotation"] > 360:
+            positionData["rotation"] = positionData["rotation"] - 360
+
+
 
 def findTurnTime(deg):
-    return deg/(angularSpeed/2) # Original Equation Is change in theta = 0.5 * (wi + w) * t
+    return deg/((zoombaStats["speed"]*zoombaStats["radius"])/2.0) # Original Equation Is change in theta = 0.5 * (wi + w) * t
+
+
+
 
 def updateSpeedAndAcceleration():           #This should be run on a separate thread
-    while True:
         changeInTime = 0.01
         getAcceleration()
         moving.speed = moving.speed + moving.acceleration * changeInTime   #Vf = vi + a*t
 
-        json = readJson("zoombaStats.json")
-        json["speed"] = moving.speed
+        zoombaStats["speed"] = moving.speed
         writeJson("zoombaStats.json", json)
 
         time.sleep(changeInTime)
@@ -52,10 +75,47 @@ def getAcceleration():
     moving.acceleration = readJson("zoombaStats.json")["acceleration"]
 
 
-def deccelerate():
-    # Normally Code to Make the Zoomba Decelerate Would go here, but this is temp Code
+
+
+
+def turnOnLeftWheels(direction): #Code To Turn On Left Wheels Would Go Here - Should NEVER be used in isolation
+    if direction == "forward":
+        #Code To Go Forward Goes Here
+        testWow = 1
+    elif direction == "backwards":
+        #Code To Go Backwards Goes here
+        testWow = 1
+
+def turnOnRightWheels(direction): #Code To Turn On Right Wheels Would Go Here - Should NEVER be used in isolation
+    if direction == "forward":
+        #Code To Go Forward Goes Here
+        testWow = 1
+    elif direction == "backwards":
+        #Code To Go Backwards Goes here
+        testWow = 1
+
+
+
+
+def cycle():
+    global zoombaStats
+    global positionData
+    wow = True
+    while True:
+        zoombaStats = readJson("zoombaStats.json")
+        positionData = readJson("positionData.json")
+
+
+
+        if wow:
+            turnRight(100)
+            wow = False
+        writeJson("positionData.json",positionData)
+
 
 if __name__ == "__main__":
     #options = getArgs()
     #statsPath = options.statsPath
     pass
+
+cycle()
