b74a6d447ac168566a70ed92eb655623325b52f4
GuyMan1020 <aydencol@hotmail.com>
2020-07-20 22:42:10 -0400

    Update accelerometer.py
    Made it work with the real thing

--- accelerometer.py
+++ accelerometer.py
@@ -1,19 +1,54 @@
-#!/usr/bin/env python3
-import movement
+# Distributed with a free-will license.
+# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+# MMA8452Q
+# This code is designed to work with the MMA8452Q_I2CS I2C Mini Module available from ControlEverything.com.
+# https://www.controleverything.com/content/Accelorometer?sku=MMA8452Q_I2CS#tabs-0-product_tabset-2
 
-#This program will be used to get data from the accelerometer pretty much
-class staticClass:
-    pass
-static = staticClass()
-static.acceleration = [1,1]
+import smbus
+import time
 
-def getAcceleration():  #This will get acceleration from the thing but for now its just one
-    return static.acceleration
+# Get I2C bus
+bus = smbus.SMBus(1)
 
-def writeAcceleration():
-    json = movement.readJson("zoombaStats.json")
-    json["acceleration"] = getAcceleration()
-    movement.writeJson("zoombaStats.json", json)
+# MMA8452Q address, 0x1C(28)
+# Select Control register, 0x2A(42)
+#       0x00(00)    StandBy mode
+bus.write_byte_data(0x1d, 0x2A, 0x00)
+# MMA8452Q address, 0x1C(28)
+# Select Control register, 0x2A(42)
+#       0x01(01)    Active mode
+bus.write_byte_data(0x1d, 0x2A, 0x01)
+# MMA8452Q address, 0x1C(28)
+# Select Configuration register, 0x0E(14)
+#       0x00(00)    Set range to +/- 2g
+bus.write_byte_data(0x1d, 0x0E, 0x00)
 
-def setAcceleration(number):        #TEMPERARY FOR NOW BECAUSE THE ACCELEROMETER WILL DO THIS
-    static.acceleration = number
+time.sleep(0.5)
+
+while True:
+        # MMA8452Q address, 0x1d(28)
+        # Read data back from 0x00(0), 7 bytes
+        # Status register, X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB
+        data = bus.read_i2c_block_data(0x1d, 0x00, 7)
+
+        # Convert the data
+        xAccl = (data[1] * 256 + data[2]) / 16
+        if xAccl > 2047 :
+            xAccl -= 4096
+
+        yAccl = (data[3] * 256 + data[4]) / 16
+        if yAccl > 2047 :
+            yAccl -= 4096
+
+        zAccl = (data[5] * 256 + data[6]) / 16
+        if zAccl > 2047 :
+            zAccl -= 4096
+
+
+        # Output data to screen
+        print("Acceleration in X-Axis : %d" %xAccl)
+        print("Acceleration in Y-Axis : %d" %yAccl)
+        print("Acceleration in Z-Axis : %d" %zAccl)
+        print("                          ")
+        time.sleep(0.5) 
+Run
